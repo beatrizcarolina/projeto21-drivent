2ae23d1efe2cf43afaa6218521e60395149c187a
beatrizcarolina <beatrizcarolinasr@gmail.com>
2023-09-18 14:34:33 -0300

    feat: include cep adress protocol

--- src/protocols.ts
+++ src/protocols.ts
@@ -10,3 +10,11 @@ export type RequestError = {
   name: string;
   message: string;
 };
+
+export type CEPadress = {
+  logradouro: string;
+  complemento: string;
+  bairro: string;
+  cidade: string;
+  uf: string;
+};
