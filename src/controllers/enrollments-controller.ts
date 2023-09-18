6b2ab39ef797b54717c1d8a93d07ae54df68c57f
beatrizcarolina <beatrizcarolinasr@gmail.com>
2023-09-18 14:49:57 -0300

    feat: get adress from cep function

--- src/controllers/enrollments-controller.ts
+++ src/controllers/enrollments-controller.ts
@@ -20,8 +20,9 @@ export async function postCreateOrUpdateEnrollment(req: AuthenticatedRequest, re
   return res.sendStatus(httpStatus.OK);
 }
 
-// TODO - Receber o CEP do usuário por query params.
+
 export async function getAddressFromCEP(req: AuthenticatedRequest, res: Response) {
-  const address = await enrollmentsService.getAddressFromCEP();
+  const cep = req.query.cep as string;
+  const address = await enrollmentsService.getAddressFromCEP(cep);
   res.status(httpStatus.OK).send(address);
 }
