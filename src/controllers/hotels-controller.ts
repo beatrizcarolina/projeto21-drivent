d328da0720800681f35945c326f489c5c92c0845
beatrizcarolina <beatrizcarolinasr@gmail.com>
2023-10-02 15:24:32 -0300

    feat: getHotels and getHotelById functions

---
+++ src/controllers/hotels-controller.ts
@@ -0,0 +1,17 @@
+import { Response } from 'express';
+import httpStatus from 'http-status';
+import { AuthenticatedRequest } from '@/middlewares';
+import { hotelsService } from '@/services';
+
+export async function getHotels(req: AuthenticatedRequest, res: Response) {
+  const { userId } = req;
+  const hotels = await hotelsService.getHotels(userId);
+  return res.status(httpStatus.OK).send(hotels);
+}
+
+export async function getHotelById(req: AuthenticatedRequest, res: Response) {
+  const { userId } = req;
+  const { hotelId } = req.params;
+  const result = await hotelsService.getHotelById(userId, Number(hotelId));
+  return res.status(httpStatus.OK).send(result);
+}
