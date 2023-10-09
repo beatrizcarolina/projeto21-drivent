0f2e77e1ac22a8aca3ba86afb4af07499b40419b
beatrizcarolina <beatrizcarolinasr@gmail.com>
2023-10-09 13:30:12 -0300

    feat: add booking test

---
+++ tests/factories/booking-factory.ts
@@ -0,0 +1,14 @@
+import { prisma } from "@/config";
+
+export async function createBooking(userId: number, roomId: number) {
+    return await prisma.booking.create({
+        data: {
+            userId,
+            roomId,
+        },
+        select: {
+            id: true,
+            Room: true,
+        },
+    });
+}
