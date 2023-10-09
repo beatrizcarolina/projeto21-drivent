dbe257654030e577c9e9ab88dc58d186e596430d
beatrizcarolina <beatrizcarolinasr@gmail.com>
2023-10-09 11:20:56 -0300

    feat: create, get and update booking functions

---
+++ src/controllers/booking-controller.ts
@@ -0,0 +1,25 @@
+import { AuthenticatedRequest } from "@/middlewares";
+import { bookingService } from "@/services";
+import { Response } from "express";
+import httpStatus from "http-status";
+
+export async function getBooking(req: AuthenticatedRequest, res: Response) {
+    const { userId } = req;
+    const booking = await bookingService.getByUserId(userId);
+    return res.status(httpStatus.OK).send(booking);
+}
+
+export async function createBooking(req: AuthenticatedRequest, res: Response) {
+    const { userId } = req;
+    const { roomId } = req.body;
+    const newBooking = await bookingService.createBooking(userId, Number(roomId));
+    return res.status(httpStatus.CREATED).send(newBooking);
+}
+
+export async function updateBooking( req: AuthenticatedRequest, res: Response) {
+    const { userId } = req;
+    const { roomId } = req.body;
+    const { bookingId } = req.params;
+    const updateBookingId = await bookingService.updateBooking(userId, Number(bookingId), Number(roomId));
+    return res.status(httpStatus.OK).send(updateBookingId);
+}
