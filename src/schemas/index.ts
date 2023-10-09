cf545ae072d0c79b509ff154b318311912e5d6ee
beatrizcarolina <beatrizcarolinasr@gmail.com>
2023-10-09 11:24:42 -0300

    feat: booking schema

--- src/schemas/index.ts
+++ src/schemas/index.ts
@@ -1,3 +1,6 @@
 export * from './users-schemas';
 export * from './authentication-schemas';
 export * from './enrollments-schemas';
+export * from './tickets-schemas';
+export * from './payments-schemas';
+export * from './booking-schemas';
